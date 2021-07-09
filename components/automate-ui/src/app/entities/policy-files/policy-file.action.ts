e5d2061c604269453f7d9f5a60db0567ec572108
Himanshi Chhabra <hchhabra@progress.com>
2021-07-09 23:16:07 +0530

    Added Policy file details Page.
    Signed-off-by: Himanshi Chhabra <hchhabra@progress.com>

--- components/automate-ui/src/app/entities/policy-files/policy-file.action.ts
+++ components/automate-ui/src/app/entities/policy-files/policy-file.action.ts
@@ -3,9 +3,12 @@ import { Action } from '@ngrx/store';
 import { PolicyFile } from './policy-file.model';
 
 export enum PolicyFileActionTypes {
-  GET_ALL = 'POLICYFILES::GET_ALL',
+  GET_ALL         = 'POLICYFILES::GET_ALL',
   GET_ALL_SUCCESS = 'POLICYFILES::GET_ALL::SUCCESS',
-  GET_ALL_FAILURE = 'POLICYFILES::GET_ALL::FAILURE'
+  GET_ALL_FAILURE = 'POLICYFILES::GET_ALL::FAILURE',
+  GET             = 'POLICYFILES::GET',
+  GET_SUCCESS     = 'POLICYFILES::GET::SUCCESS',
+  GET_FAILURE     = 'POLICYFILES::GET::FAILURE'
 }
 
 export interface PolicyFilesSuccessPayload {
@@ -27,7 +30,26 @@ export class GetPolicyFilesFailure implements Action {
   constructor(public payload: HttpErrorResponse) { }
 }
 
+export class GetPolicyFile implements Action {
+  readonly type = PolicyFileActionTypes.GET;
+  constructor(public payload: {
+    server_id: string, org_id: string, name: string, revision: string }) { }
+}
+
+export class GetPolicyFileSuccess implements Action {
+  readonly type = PolicyFileActionTypes.GET_SUCCESS;
+  constructor(public payload: PolicyFile) { }
+}
+
+export class GetPolicyFileFailure implements Action {
+  readonly type = PolicyFileActionTypes.GET_FAILURE;
+  constructor(public payload: HttpErrorResponse) { }
+}
+
 export type PolicyFileActions =
   | GetPolicyFiles
   | GetPolicyFilesSuccess
-  | GetPolicyFilesFailure;
+  | GetPolicyFilesFailure
+  | GetPolicyFile
+  | GetPolicyFileSuccess
+  | GetPolicyFileFailure;
--- components/automate-ui/src/app/entities/policy-files/policy-file.effects.ts
+++ components/automate-ui/src/app/entities/policy-files/policy-file.effects.ts
@@ -11,6 +11,9 @@ import {
   GetPolicyFilesSuccess,
   PolicyFilesSuccessPayload,
   GetPolicyFilesFailure,
+  GetPolicyFile,
+  GetPolicyFileSuccess,
+  GetPolicyFileFailure,
   PolicyFileActionTypes
 } from './policy-file.action';
 
@@ -42,5 +45,25 @@ export class PolicyFileEffects {
           message: `Could not get policy files: ${msg || payload.error}`
         });
       })));
+
+  getPolicyFile$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(PolicyFileActionTypes.GET),
+      mergeMap(({ payload: { server_id, org_id, name, revision } }: GetPolicyFile) =>
+        this.requests.getPolicyFile(server_id, org_id, name, revision).pipe(
+          map((resp) => new GetPolicyFileSuccess(resp)),
+          catchError((error: HttpErrorResponse) =>
+          observableOf(new GetPolicyFileFailure(error)))))));
+
+  getPolicyFileFailure$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(PolicyFileActionTypes.GET_FAILURE),
+      map(({ payload }: GetPolicyFileFailure) => {
+        const msg = payload.error.error;
+        return new CreateNotification({
+          type: Type.error,
+          message: `Could not get policy file: ${msg || payload.error}`
+        });
+    })));
 
 }
